a6af25a45cfe6597f5d4958e48b3158dcdea0e71
Bruno Silva <bruno_bls23@hotmail.com>
2022-05-04 08:12:51 -0300

    New module controller implements

--- src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -1,6 +1,7 @@
 package com.ead.course.repositories;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import com.ead.course.models.ModuleModel;
@@ -14,4 +15,7 @@ public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
     // @Modifying para alterações no bd
     @Query(value = "select * from modules where course_course_id = :courseId", nativeQuery = true)
     List<ModuleModel> findAllModulesIntoCourse(@Param("courseId") UUID courseId);
+
+    @Query(value = "select * from modules where course_course_id = :courseId and module_id = :moduleId", nativeQuery = true)
+    Optional<ModuleModel> findModuleIntoCourse(@Param("courseId") UUID courseId, @Param("moduleId") UUID moduleId);
 }
--- src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ead.course.services.impl;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import com.ead.course.models.LessonModel;
 import com.ead.course.models.ModuleModel;
@@ -31,5 +33,15 @@ public class ModuleServiceImpl implements ModuleService{
         }
 
         moduleRepository.delete(moduleModel);        
+    }
+
+    @Override
+    public ModuleModel save(ModuleModel moduleModel) {
+        return moduleRepository.save(moduleModel);
+    }
+
+    @Override
+    public Optional<ModuleModel> findModuleIntoCourse(UUID courseId, UUID moduleId) {
+        return moduleRepository.findModuleIntoCourse(courseId, moduleId);
     }
 }
