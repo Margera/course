e2fe2062a23baf5e236742511fe56985d3aa9bd4
Bruno Silva <bruno_bls23@hotmail.com>
2022-05-05 13:34:20 -0300

    New Lessons controller
    Added lessons controller

--- src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
@@ -1,5 +1,10 @@
 package com.ead.course.services.impl;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.ead.course.models.LessonModel;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.services.LessonService;
 
@@ -11,4 +16,24 @@ public class LessonServiceImpl implements LessonService{
     
     @Autowired
     LessonRepository lessonRepository;
+
+    @Override
+    public LessonModel save(LessonModel lessonModel) {
+        return lessonRepository.save(lessonModel);
+    }
+
+    @Override
+    public Optional<LessonModel> findLessonIntoModule(UUID moduleId, UUID lessonId) {
+        return lessonRepository.findLessonIntoModule(moduleId, lessonId);
+    }
+
+    @Override
+    public void delete(LessonModel lessonModel) {
+        lessonRepository.delete(lessonModel);        
+    }
+
+    @Override
+    public List<LessonModel> findAllByModule(UUID moduleId) {
+        return lessonRepository.findAllLessonsIntoCourse(moduleId);
+    }
 }
