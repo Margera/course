078e3bf35d3179a6ff942d85a4cc4457a3b648f0
Bruno Silva <bruno_bls23@hotmail.com>
2022-05-08 16:51:23 -0300

    Apply ISO 8601 UTC
    Modify dates to ISO 8601 UTC

---
+++ src/main/java/com/ead/course/configs/DateConfig.java
@@ -0,0 +1,27 @@
+package com.ead.course.configs;
+
+import java.time.format.DateTimeFormatter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+// Exemplo de configuração de nivel global
+//@Configuration
+public class DateConfig {
+
+    public static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+    public static LocalDateTimeSerializer lOCAL_DATETIME_SERIALIZER = new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT));   
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper(){
+        JavaTimeModule module = new JavaTimeModule();
+        module.addSerializer(lOCAL_DATETIME_SERIALIZER);
+        return new ObjectMapper().registerModule(module);
+    }
+}
