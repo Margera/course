1f4c93467035c55d8de1b35957dab7cf8d535a80
Bruno Silva <bruno_bls23@hotmail.com>
2022-05-24 13:23:33 -0300

    Add RestTemplate CourseClient

---
+++ src/main/java/com/ead/course/clients/CourseClient.java
@@ -0,0 +1,14 @@
+package com.ead.course.clients;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class CourseClient {
+    
+    @Autowired
+    RestTemplate restTemplate;
+
+    10:20
+}
