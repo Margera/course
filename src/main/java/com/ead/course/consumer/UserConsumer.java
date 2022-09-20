0e05c6e11de5e0eaf78e29c38b03ae5fde0a1795
Bruno Silva <bruno_bls23@hotmail.com>
2022-09-20 14:10:32 -0300

    update rabbtMq configs

--- src/main/java/com/ead/course/consumer/UserConsumer.java
+++ src/main/java/com/ead/course/consumer/UserConsumer.java
@@ -17,15 +17,12 @@ import com.ead.course.services.UserService;
 public class UserConsumer {
 
     @Autowired
-    UserService userService;
-  
-
+    UserService userService; 
     
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue(value = "${ead.broker.queue.userEventQueue.name}", durable = "true"),
             exchange = @Exchange(value = "${ead.broker.exchange.userEventExchange}", type = ExchangeTypes.FANOUT, ignoreDeclarationExceptions = "true"))
     )
-
     public void listenUserEvent(@Payload UserEventDto userEventDto){
         var userModel = userEventDto.convertToUserModel();
 
